3c5adf8bb31c4d18d923ff977280b0294458f3c0
SeveriusRasmussen <sora0001@stud.kea.dk>
2025-01-05 14:27:38 +0100

    Working with some stuffs

--- src/Semester2/Wrapper/WrapperClass.java
+++ src/Semester2/Wrapper/WrapperClass.java
@@ -10,5 +10,15 @@ public class WrapperClass {
         System.out.println(myInt);
         System.out.println(myDouble);
         System.out.println(myChar);
+
+        // This will output the same result as above
+        System.out.println(myInt.intValue());
+        System.out.println(myDouble.doubleValue());
+        System.out.println(myChar.charValue());
+
+        // toString is useful for convert wrapper class objects to strings
+        Integer myInt2 = 100;
+        String myString = myInt2.toString();
+        System.out.println(myString.length());
     }
 }
